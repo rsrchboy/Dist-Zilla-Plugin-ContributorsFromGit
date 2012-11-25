228b4a4158ccec4571ad354cd8c7218451135ac3
Chris Weyl <cweyl@alumni.drew.edu>
2012-11-25 01:29:49 -0800

    add/clean pod, comments

--- lib/Dist/Zilla/Plugin/ContributorsFromGit.pm
+++ lib/Dist/Zilla/Plugin/ContributorsFromGit.pm
@@ -21,18 +21,6 @@ with
 # debugging...
 #use Smart::Comments '###';
 
-# all author names
-# git log --format="%aN <%aE>" | sort | uniq
-# all contributor
-# git log --format="%cN <%cE>" | sort | uniq
-
-# per-line stats, too; see
-# http://codeimpossible.com/2011/12/16/Stupid-Git-Trick-getting-contributor-stats/
-
-# XXX add PodWeaver stash if not found?
-# XXX add git committers into the mix, too?
-# XXX make zilla->authors filtering optional?
-
 sub before_build {
     my $self = shift @_;
 
@@ -61,13 +49,42 @@ __PACKAGE__->meta->make_immutable;
 !!42;
 __END__
 
+=for :stopwords zilla
+
+=for Pod::Coverage before_build
+
 =head1 SYNOPSIS
+
+    ; in your dist.ini
+    [ContributorsFromGit]
+
+    ; in your weaver.ini
+    [Contributors]
 
 =head1 DESCRIPTION
 
+This plugin makes it easy to acknowledge the contributions of others by
+populating a L<%PodWeaver|Dist::Zilla::Stash::PodWeaver> stash with the unique
+list of all git commit authors reachable from the current HEAD.
+
+=head1 OVERVIEW
+
+On collecting the unique list of reachable commit authors from git, we search
+and remove any git authors from the list of authors L<Dist::Zilla> knows
+about.  We then look for a stash named C<%PodWeaver>; if we don't find one
+then we create an instance of L<Dist::Zilla::Stash::PodWeaver> and register it
+with our zilla instance.  Then we add the list of contributors (the filtered
+git authors list) to the stash in such a way that
+L<Pod::Weaver::Section::Contributors> can find them.
+
+Note that you do not need to have the C<%PodWeaver> stash created; it will be
+added if it is not found.  However, your L<Pod::Weaver> config (aka
+c<weaver.ini>) must include the
+L<Contributors|Pod::Weaver::Section::Contributors> section plugin.
+
 =head1 SEE ALSO
 
-L<Pod::Weaver::Section::Contributor>
+L<Pod::Weaver::Section::Contributors>
 
 L<Dist::Zilla::Stash::PodWeaver>
 
